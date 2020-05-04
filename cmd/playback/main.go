4d382282ef05b261b2913eaa704970ed3aae6df5
Ed Welch <edward.welch@grafana.com>
2020-05-04 10:43:36 -0400

    replaying metrics kind of working

--- cmd/playback/main.go
+++ cmd/playback/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
@@ -17,13 +18,16 @@ import (
 	"time"
 
 	"github.com/grafana/loki/pkg/loghttp"
+	"github.com/prometheus/client_golang/api"
+	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/prometheus/common/model"
 
 	"github.com/slim-bean/leafbus/pkg/stream"
 )
 
 func main() {
-
-	sc := newSynchroinzer()
+	controlChannel := make(chan int)
+	sc := newSynchroinzer(controlChannel)
 
 	imageServer := &imageServer{
 		sc,
@@ -54,7 +58,7 @@ func main() {
 		}
 		if strings.ToLower(run) == "true" {
 			log.Println("Starting Services from HTTP Request")
-			sc.run(start, end, 1)
+			sc.run(start, end, 5)
 			w.WriteHeader(http.StatusOK)
 			return
 		}
@@ -65,28 +69,26 @@ func main() {
 		log.Println(err)
 	}
 
-	//log.Println("Sleeping for 15s")
-	//time.Sleep(15 * time.Second)
-	//log.Println("Done Sleeping, querying")
-
 }
 
 type synchronizer struct {
-	syncChannels []chan *time.Time
-	syncMtx      sync.Mutex
+	controlChanel chan int
+	syncChannels  []chan *time.Time
+	syncMtx       sync.Mutex
 }
 
-func newSynchroinzer() *synchronizer {
+func newSynchroinzer(controlChanel chan int) *synchronizer {
 	return &synchronizer{
-		syncChannels: []chan *time.Time{},
-		syncMtx:      sync.Mutex{},
+		controlChanel: controlChanel,
+		syncChannels:  []chan *time.Time{},
+		syncMtx:       sync.Mutex{},
 	}
 }
 
 func (s *synchronizer) addSyncChannel() chan *time.Time {
 	s.syncMtx.Lock()
 	defer s.syncMtx.Unlock()
-	c := make(chan *time.Time)
+	c := make(chan *time.Time, 10)
 	s.syncChannels = append(s.syncChannels, c)
 	return c
 }
@@ -112,26 +114,35 @@ func (s *synchronizer) removeSyncChannel(c chan *time.Time) {
 func (s *synchronizer) run(start, end time.Time, scale int64) {
 	next := start
 	lastSent := time.Unix(0, 0)
+	ticker := time.NewTicker(1 * time.Millisecond)
 	for {
-		time.Sleep(1 * time.Millisecond)
-		now := time.Now()
-		elapsed := now.Sub(lastSent).Milliseconds()
-		// Default 10ms tick for timestamps
-		if elapsed < 10/scale {
-			continue
-		}
-		lastSent = now
-		// Send time to channels
-		s.syncMtx.Lock()
-		for _, c := range s.syncChannels {
-			c <- &next
-		}
-		s.syncMtx.Unlock()
-		// Add the fixed 10ms to the counter
-		next = next.Add(10 * time.Millisecond)
-		if next.After(end) {
-			log.Println("Reached end timestamp")
-			break
+		select {
+		case <-ticker.C:
+			time.Sleep(1 * time.Millisecond)
+			now := time.Now()
+			elapsed := now.Sub(lastSent).Milliseconds()
+			// Default 10ms tick for timestamps
+			if elapsed < 10/scale {
+				continue
+			}
+			lastSent = now
+			// Send time to channels
+			s.syncMtx.Lock()
+			for _, c := range s.syncChannels {
+				if len(c) < 10 {
+					c <- &next
+				} else {
+					log.Println("A sender is not keeping up, TS dropped")
+				}
+
+			}
+			s.syncMtx.Unlock()
+			// Add the fixed 10ms to the counter
+			next = next.Add(10 * time.Millisecond)
+			if next.After(end) {
+				log.Println("Reached end timestamp")
+				break
+			}
 		}
 	}
 }
@@ -315,7 +326,149 @@ type metricServer struct {
 }
 
 func (s *metricServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	name := r.Form.Get("name")
+	if name == "" {
+		http.Error(w, "Missing name parameter", http.StatusBadRequest)
+		return
+	}
 
+	var rate time.Duration
+	rateQuery := r.Form.Get("rate")
+	if rateQuery != "" && rateQuery != "undefined" {
+		rt, err := strconv.ParseInt(rateQuery, 10, 64)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Unable to parse rate as int64: %v", err), http.StatusBadRequest)
+			return
+		}
+		rate = time.Duration(rt) * time.Millisecond
+	}
+
+	start, end, err := bounds(r)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	syncChan := s.sc.addSyncChannel()
+	defer func() {
+		s.sc.removeSyncChannel(syncChan)
+	}()
+	go s.metricLoader(start, end, name)
+
+	w.Header().Set("Content-Type", "application/json")
+	enc := json.NewEncoder(w)
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		panic("expected http.ResponseWriter to be an http.Flusher")
+	}
+
+	var currEntry *stream.Data
+	lastTimestamp := time.Unix(0, 0)
+
+	for {
+		select {
+		case nextTimestamp := <-syncChan:
+			for {
+				time.Sleep(1 * time.Millisecond)
+				// Dequeue next entry
+				if currEntry == nil {
+					b, ok := <-s.c
+					if !ok {
+						log.Println("Sender out of data, channel closed")
+						break
+					}
+					currEntry = b
+				}
+				currentTimestamp := time.Unix(0, currEntry.Timestamp*int64(1e6))
+
+				// Need to wait for next timestamp to catch up, keep point but continue
+				if currentTimestamp.After(*nextTimestamp) {
+					break
+				}
+
+				// Throw this point away because the requested rate is less than we are receiving images
+				if currentTimestamp.Before(lastTimestamp.Add(rate)) {
+					currEntry = nil
+					break
+				}
+
+				//log.Println("Sending Metric:", name, "Value:", currentTimestamp)
+				err := enc.Encode(currEntry)
+				if err != nil {
+					log.Println("Failed to marshal data object to json stream:", err)
+				}
+				flusher.Flush()
+
+				lastTimestamp = currentTimestamp
+				currEntry = nil
+				break
+			}
+		}
+	}
+}
+
+func (s *metricServer) metricLoader(start, end time.Time, queryString string) {
+	lastSent := time.Unix(0, 0)
+	client, err := api.NewClient(api.Config{
+		Address: "http://localhost:8002/api/prom",
+	})
+	if err != nil {
+		fmt.Printf("Error creating client: %v\n", err)
+		return
+	}
+	v1api := v1.NewAPI(client)
+	for {
+		// ?query=battery_amps&start=1588515010&end=1588516734&step=2
+		adjustedEnd := start.Add(1 * time.Minute)
+		r := v1.Range{
+			Start: start,
+			End:   adjustedEnd,
+			Step:  10 * time.Millisecond,
+		}
+		log.Println("Querying:", queryString, "Range:", r)
+		result, warnings, err := v1api.QueryRange(context.Background(), queryString, r)
+		if err != nil {
+			log.Printf("Error querying Prometheus: %v\n", err)
+			return
+		}
+		if len(warnings) > 0 {
+			log.Printf("Warnings: %v\n", warnings)
+		}
+
+		if m, ok := result.(model.Matrix); ok {
+			for i, st := range m {
+				if st.Metric.String() == queryString {
+					for j, entry := range st.Values {
+						if !entry.Timestamp.After(model.TimeFromUnixNano(lastSent.UnixNano())) {
+							log.Println("ignoring old entry")
+							continue
+						}
+						//log.Println("Pushing image to queue with time:", entry.Timestamp)
+						d := &stream.Data{
+							Name:      queryString,
+							Timestamp: m[i].Values[j].Timestamp.UnixNano() / 1e6,
+							Val:       float64(m[i].Values[j].Value),
+						}
+
+						s.c <- d
+						lastSent = entry.Timestamp.Time()
+						start = entry.Timestamp.Time()
+					}
+				}
+			}
+		}
+		// Wait for buffer to be less than half full
+		for len(s.c) > 250 {
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
 }
 
 func bounds(r *http.Request) (time.Time, time.Time, error) {
