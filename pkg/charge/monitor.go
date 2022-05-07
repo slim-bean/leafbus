58fe6fa06706d1604ee39f25f3087db70a845f64
Edward Welch <edward.welch@grafana.com>
2022-05-07 08:31:23 -0400

    only stop charging when it's the leaf which is charging.

--- pkg/charge/monitor.go
+++ pkg/charge/monitor.go
@@ -2,14 +2,23 @@ package charge
 
 import (
 	"log"
+	"sync"
 	"time"
 
 	"github.com/brutella/can"
 )
 
+type battCurrent struct {
+	current int16
+	time    time.Time
+}
+
 type Monitor struct {
-	charger    *openevse
-	currCharge uint16
+	charger     *openevse
+	currCharge  uint16
+	battCurrent battCurrent
+	time        time.Time
+	mtx         sync.Mutex
 }
 
 func NewMonitor(chargerAddress string) (*Monitor, error) {
@@ -26,10 +35,26 @@ func NewMonitor(chargerAddress string) (*Monitor, error) {
 
 func (m *Monitor) Handle(frame can.Frame) {
 	// Only care about current charge status
-	if frame.ID != 0x55B {
-		return
+	switch frame.ID {
+	case 0x55B:
+		m.mtx.Lock()
+		m.currCharge = (uint16(frame.Data[0]) << 2) | (uint16(frame.Data[1]) >> 6)
+		m.mtx.Unlock()
+	case 0x1DB:
+		//Battery Current and Voltage
+		var battCurrent int16
+		if frame.Data[0]&0b10000000 == 0b10000000 {
+			battCurrent = int16((uint16(frame.Data[0]) << 3) | 0b1111100000000000 | uint16(frame.Data[1]>>6))
+		} else {
+			battCurrent = int16((uint16(frame.Data[0])<<3)&0b0000011111111111 | uint16(frame.Data[1]>>6))
+		}
+		m.mtx.Lock()
+		m.battCurrent.current = battCurrent
+		// We don't need a lot of accuracy for this time so we use a time value updated every 10 seconds
+		// in the run() loop instead of doing time.Now for every current update
+		m.battCurrent.time = m.time
+		m.mtx.Unlock()
 	}
-	m.currCharge = (uint16(frame.Data[0]) << 2) | (uint16(frame.Data[1]) >> 6)
 }
 
 func (m *Monitor) run() {
@@ -37,6 +62,9 @@ func (m *Monitor) run() {
 	for {
 		select {
 		case <-t.C:
+			m.mtx.Lock()
+			m.time = time.Now()
+			m.mtx.Unlock()
 			log.Println("Current Charge:", m.currCharge)
 			st, err := m.charger.sendCommand(query)
 			if err != nil {
@@ -44,7 +72,10 @@ func (m *Monitor) run() {
 				continue
 			}
 			log.Println(st)
-			if st == charging && m.currCharge >= 780 {
+			m.mtx.Lock()
+			shouldStop := st == charging && m.currCharge >= 780 && time.Since(m.battCurrent.time) < 1*time.Minute && m.battCurrent.current > 5
+			m.mtx.Unlock()
+			if shouldStop {
 				log.Println("Reached charge limit, stopping charging")
 				_, err := m.charger.sendCommand(sleep)
 				if err != nil {
